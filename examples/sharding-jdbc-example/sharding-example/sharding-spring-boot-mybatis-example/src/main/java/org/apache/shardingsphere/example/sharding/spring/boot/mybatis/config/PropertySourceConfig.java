3c4c5dee6b5437f6a5549c8b366bc20e7ba972af
unknown <YC02167@mbcoffice.com>
2021-12-24 16:47:51 +0800

    first

---
+++ examples/sharding-jdbc-example/sharding-example/sharding-spring-boot-mybatis-example/src/main/java/org/apache/shardingsphere/example/sharding/spring/boot/mybatis/config/PropertySourceConfig.java
@@ -0,0 +1,8 @@
+package org.apache.shardingsphere.example.sharding.spring.boot.mybatis.config;
+
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PropertySourceConfig {
+}
